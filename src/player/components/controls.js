51fc99ce9835cc9f0ca5481cbcf2a388c81a84e4
Francisco Figuereo <ffiguereo2@gmail.com>
2018-01-14 23:36:58 +0100

    controls component

---
+++ src/player/components/controls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './controls.css';
+
+const Controls = (props) => (
+    <div className="Controls">
+        {props.children}
+    </div>
+)
+
+export default Controls;
